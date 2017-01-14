3a27524a8560b1502e14192f0fa5383faf4ed5ec
Sam.Shen <samfec@163.com>
2017-01-14 21:26:14 +0800

    添加echarts柱状图

--- static/js/mian.js
+++ static/js/mian.js
@@ -1,20 +1,58 @@
 import echarts from 'echarts';
 import '../sass/main.css';
 
-
-
 let myChart = echarts.init(window.document.getElementById('app'));
-// 绘制图表
-myChart.setOption({
-    title: { text: 'ECharts 入门示例' },
+
+const companies = ['Tesla', 'Google', 'Apple', 'Amazon', 'Facebook'];
+const xAxisData = ["1月"];
+const seriesData = {
+    'Tesla': [237.75],
+    'Google': [807.88],
+    'Apple': [119.04],
+    'Amazon': [817.14],
+    'Facebook': [128.34]
+};
+let series = [];
+for (let i = 0; i < companies.length; i++) {
+    let s = {
+        name: companies[i],
+        type: 'bar',
+        data: seriesData[companies[i]],
+        label: {
+            normal: {
+                show: true,
+                position: 'top'
+            }
+        },
+        // markLine: {
+        //     data: [
+        //         {type: 'average', name: '平均值'}
+        //     ]
+        // }
+    };
+    series.push(s);
+}
+const option = {
+    title: {
+        text: '研究五家公司股价趋势',
+        x: 'center',
+        y: 0,
+    },
+    legend: {
+        data: companies,
+        // align: 'right',
+        // right: 10
+        orient: 'vertical',
+        right: 'right',
+    },
     tooltip: {},
     xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        data: xAxisData
     },
-    yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-});
+    yAxis: {
+        name: '股价（USD）'
+    },
+    series: series,
+};
+
+myChart.setOption(option);
