df49e5d714f908023326db720f6f665dd723fa20
Sam.Shen <samfec@163.com>
2017-07-23 15:44:01 +0800

    添加7月23日的股票数据

--- static/js/mian.js
+++ static/js/mian.js
@@ -4,55 +4,55 @@ import '../sass/main.css';
 let myChart = echarts.init(window.document.getElementById('app'));
 
 const companies = ['Tesla', 'Google', 'Apple', 'Amazon', 'Facebook'];
-const xAxisData = ["1月"];
+const xAxisData = ['2017年1月', '2017年7月'];
 const seriesData = {
-    'Tesla': [237.75],
-    'Google': [807.88],
-    'Apple': [119.04],
-    'Amazon': [817.14],
-    'Facebook': [128.34]
+    'Tesla': [237.75, 328.40,],
+    'Google': [807.88, 972.92,],
+    'Apple': [119.04, 150.27,],
+    'Amazon': [817.14, 1025.67,],
+    'Facebook': [128.34, 163.43,]
 };
 let series = [];
 for (let i = 0; i < companies.length; i++) {
     let s = {
         name: companies[i],
-        type: 'bar',
+        type: 'line',
         data: seriesData[companies[i]],
-        label: {
-            normal: {
-                show: true,
-                position: 'top'
-            }
-        },
-        // markLine: {
-        //     data: [
-        //         {type: 'average', name: '平均值'}
-        //     ]
-        // }
+        stack: '总量',
     };
     series.push(s);
 }
+
 const option = {
     title: {
-        text: '研究五家公司股价趋势',
-        x: 'left',
-        y: 0,
+        text: '研究五家公司股价趋势'
+    },
+    tooltip: {
+        trigger: 'axis'
     },
     legend: {
-        data: companies,
-        // align: 'right',
-        // right: 10
-        orient: 'vertical',
-        right: 'right',
+        data: companies
     },
-    tooltip: {},
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
     xAxis: {
+        type: 'category',
+        boundaryGap: false,
         data: xAxisData
     },
     yAxis: {
-        name: '股价（USD）'
+        type: 'value'
     },
-    series: series,
+    series: series
 };
 
 myChart.setOption(option);
--- static/js/test.js
+++ static/js/test.js
@@ -1,6 +1,62 @@
-/**
- * Created by SamMFFL on 2017/1/3.
- */
-
-
-// $('.app').html('test');
+const option = {
+    title: {
+        text: '研究五家公司股价趋势'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['Tesla', 'Google', 'Apple', 'Amazon', 'Facebook']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['2017年1月','2017年7月']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name:'Tesla',
+            type:'line',
+            stack: '总量',
+            data:[237.75, 328.40,]
+        },
+        {
+            name:'Google',
+            type:'line',
+            stack: '总量',
+            data:[807.88, 972.92,]
+        },
+        {
+            name:'Apple',
+            type:'line',
+            stack: '总量',
+            data:[119.04, 150.27,]
+        },
+        {
+            name:'Amazon',
+            type:'line',
+            stack: '总量',
+            data:[817.14, 1025.67,]
+        },
+        {
+            name:'Facebook',
+            type:'line',
+            stack: '总量',
+            data:[128.34, 163.43,]
+        }
+    ]
+};
